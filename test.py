03c667efc9f70f6321780d2703af66779d1d5349
Leighton Swannell <leighton.swannell@icloud.com>
2020-02-03 22:01:32 +1000

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import numpy as np
+import itertools as it
+import pandas as pd
+import os
+import sys
+from collections import defaultdict
+from io import StringIO
+import random as rn
+
+data = [[]['A', 'Bm', '2'], ['A', 'Bm', '8'], ['C', 'G#m', '3']]
+
+for i in data:
+    print(i)
+
