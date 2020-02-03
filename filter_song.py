3552e34714bd2e8fdde33d88445a1c6c6eb6890a
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2020-02-03 23:43:21 +1000

    Update filter_song.py

--- filter_song.py
+++ filter_song.py
@@ -3,6 +3,8 @@ import itertools as it
 import pandas as pd
 import os
 from collections import defaultdict
+from numpy import asarray
+from numpy import save
 
 chords = ["A", "Am", "A#", "A#m",
                 "B", "Bm",
@@ -39,28 +41,28 @@ for file1 in os.listdir(sel):  #repeat for all files in major or minor
 
     answers = defaultdict(int)
 
-    for i in range(progArray.shape[0]):
+    for i in range(progArray.shape[0]): #loop through permutation of chord progressions
         s = progArray[i]
         newProgArray = listToString(s)
-        for j in range(len(chordProg) - 1):
-            if progArray[i][0] == chordProg[j] and progArray[i][1] == chordProg[j + 1]:
+        for j in range(len(chordProg) - 1): #check if song progression matches any in progArray
+            if progArray[i][0] == chordProg[j] and progArray[i][1] == chordProg[j + 1]: #add to dictionary if true
                 answers[newProgArray] += 1
-    for npA in sorted(answers.keys()):
+    for npA in sorted(answers.keys()): #export data to temp file
         print(npA, answers[npA], file=open("temp.txt", "a"))
 
-    raw = []
-    with open('temp.txt', 'r') as file:
+    raw = [] #new array
+    with open('temp.txt', 'r') as file: #open .txt file and clear newlines
         for line in file:
             raw.append(line.split())
 
-    tot = pd.DataFrame(raw, columns=['row', 'column', 'value'])
+    tot = pd.DataFrame(raw, columns=['row', 'column', 'value']) #convert into numPy array
     tot = tot.rename_axis('ID').values
     print(tot)
     dict[file1[:-4]] = tot
 
-    os.remove("temp.txt")
+    os.remove("temp.txt") # remove temp file
 
-    if sum(answers.values()) == total:
+    if sum(answers.values()) == total: #verify if number of chord progressions is number of chords - 1
         print("Total: " + str(sum(answers.values())) + "\n")
     else:
         print("Total: " + str(sum(answers.values())) + " Does not match: " + str(total) + "\n")
@@ -73,11 +75,14 @@ stackedArray = np.delete(stackedArray, 0, 0)
 
 dict2 = {} #new blank dictionary
 
-for elem in stackedArray:
+for elem in stackedArray: #remove duplicates, by using dictionary
     key = tuple(elem[:-1])
     value = int(elem[-1])
     dict2[key] = dict2.get(key,0) + value
 
-finalArray = [[*key, value] for key, value in dict2.items()]
-finalArray = np.array(finalArray)
+finalArray = [[*key, value] for key, value in dict2.items()] #convert to array
 print(finalArray)
+
+with open("export.txt", "w") as txt_file: #export to plain array txt
+    for line in finalArray:
+        txt_file.write(" ".join(map(str,line)) + "\n") # works with any number of elements in a line
