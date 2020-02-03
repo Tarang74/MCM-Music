2b06fee8e6ffb349171cb1d82934621025a8e3ab
Leighton Swannell <leighton.swannell@icloud.com>
2020-02-03 22:14:31 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-import numpy as np
-import itertools as it
-import pandas as pd
-import os
-import sys
-from collections import defaultdict
-from io import StringIO
-import random as rn
-
 data = [[]['A', 'Bm', '2'], ['A', 'Bm', '8'], ['C', 'G#m', '3']]
 
 for i in data:
