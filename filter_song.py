33912d69ccdf8dce352c3aa4ff4f254bd2f95ed5
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2020-02-03 22:02:03 +1000

    Update filter_song.py

--- filter_song.py
+++ filter_song.py
@@ -2,26 +2,9 @@ import numpy as np
 import itertools as it
 import pandas as pd
 import os
-import sys
 from collections import defaultdict
-from io import StringIO
 
-directory = "major/"
-for filename in os.listdir(directory):
-    if filename.endswith(".txt"):
-        src = os.path.join(directory, filename)
-        f = open(src, "r+")
-        print("Name of song: ", f.readline())
-
-        lines = f.readlines()
-        a = '\t'.join([line.strip() for line in lines])
-        a = a.replace('N.C.', '')
-        a = ' '.join(a.split())
-
-        f.close()
-        os.remove("output.txt")
-
-        chords = ["A", "Am", "A#", "A#m",
+chords = ["A", "Am", "A#", "A#m",
                 "B", "Bm",
                 "C", "Cm", "C#", "C#m",
                 "D", "Dm", "D#", "D#m",
@@ -29,41 +12,60 @@ for filename in os.listdir(directory):
                 "F", "Fm", "F#", "F#m",
                 "G", "Gm", "G#", "G#m"]
 
-        progArray = np.asarray(list(it.product(chords, repeat=2)))
+progArray = np.asarray(list(it.product(chords, repeat=2)))
 
-        chordProg = [w for w in a.split() if w in chords]
+dict = {}
 
-        total = len(chordProg) - 1
+def listToString(s):
+    str = " "
+    return (str.join(s))
 
-        print(str(len(chordProg)) + " chords: " + str(total) + " chord progressions\n")
+for file1 in os.listdir("major"):
+    dir = "major/" + file1
+    f = open(dir, "r+")
+    print("File: " + file1 + " - Name: " + f.readline())
 
-        def listToString(s):
-            str = " "
-            return (str.join(s))
+    lines = f.readlines()
+    a = '\t'.join([line.strip() for line in lines])
+    a = a.replace('N.C.', '')
+    a = ' '.join(a.split())
+    f.close()
 
-        answers = defaultdict(int)
+    chordProg = [w for w in a.split() if w in chords]
+    total = len(chordProg) - 1
+    #print(str(len(chordProg)) + " chords: " + str(total) + " chord progressions\n")
 
-        for i in range(progArray.shape[0]):
-            s = progArray[i]
-            newProgArray = listToString(s)
-            for j in range(len(chordProg) - 1):
-                if progArray[i][0] == chordProg[j] and progArray[i][1] == chordProg[j + 1]:
-                    answers[newProgArray] += 1
-        for npA in sorted(answers.keys()):
-            print(npA, answers[npA], file=open("output.txt", "a"))
+    answers = defaultdict(int)
 
-        raw = []
-        with open('output.txt', 'r') as file:
-            for line in file:
-                raw.append(line.split())
-        data = pd.DataFrame(raw,columns = ['row','column','value'])
-        data = data.rename_axis('ID').values
-        print(data)
-        #os.remove("output.txt")
+    for i in range(progArray.shape[0]):
+        s = progArray[i]
+        newProgArray = listToString(s)
+        for j in range(len(chordProg) - 1):
+            if progArray[i][0] == chordProg[j] and progArray[i][1] == chordProg[j + 1]:
+                answers[newProgArray] += 1
+    for npA in sorted(answers.keys()):
+        print(npA, answers[npA], file=open("output.txt", "a"))
 
-        if sum(answers.values()) == total:
-            print("Total was achieved - " + str(sum(answers.values())))
-        else:
-            print("Total was not achieved - " + str(sum(answers.values())))
+    raw = []
+    with open('output.txt', 'r') as file:
+        for line in file:
+            raw.append(line.split())
+
+    tot = pd.DataFrame(raw, columns=['row', 'column', 'value'])
+    tot = tot.rename_axis('ID').values
+    print(tot)
+    dict[file1[:-4]] = tot
+
+    os.remove("output.txt")
+
+    if sum(answers.values()) == total:
+        print("Total: " + str(sum(answers.values())) + "\n")
     else:
-        print("No songs in directory!")
+        print("Total: " + str(sum(answers.values())) + " Does not match: " + str(total) + "\n")
+
+final = np.empty(3)
+
+for i in dict:
+    final = np.vstack((final, dict[i]))
+final = np.delete(final, 0, 0)
+print(final)
