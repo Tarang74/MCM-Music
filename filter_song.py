4f9d9105b04aeb6ba36d7849c3a9cc4c9d5af57d
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2020-02-04 21:51:01 +1000

    fixed merge errors

--- filter_song.py
+++ filter_song.py
@@ -46,33 +46,30 @@ for file1 in os.listdir(sel):  #repeat for all files in major or minor
 
     answers = defaultdict(int)
 
-    for i in range(progArray.shape[0]): #loop through permutation of chord progressions
+    for i in range(progArray.shape[0]):
         s = progArray[i]
         newProgArray = listToString(s)
-        for j in range(len(chordProg) - 1): #check if song progression matches any in progArray
-            if progArray[i][0] == chordProg[j] and progArray[i][1] == chordProg[j + 1]: #add to dictionary if true
+        for j in range(len(chordProg) - 1):
+            if progArray[i][0] == chordProg[j] and progArray[i][1] == chordProg[j + 1]:
                 answers[newProgArray] += 1
-    for npA in sorted(answers.keys()): #export data to temp file
+    for npA in sorted(answers.keys()):
         print(npA, answers[npA], file=open("temp.txt", "a"))
 
-    raw = [] #new array
-    with open('temp.txt', 'r') as file: #open .txt file and clear newlines
+    raw = []
+    with open('temp.txt', 'r') as file:
         for line in file:
             raw.append(line.split())
 
-    tot = pd.DataFrame(raw, columns=['row', 'column', 'value']) #convert into numPy array
+    tot = pd.DataFrame(raw, columns=['row', 'column', 'value'])
     tot = tot.rename_axis('ID').values
     dict[file1[:-4]] = tot
 
-    os.remove("temp.txt") # remove temp file
+    os.remove("temp.txt")
 
     if sum(answers.values()) == total:
         numProgs = numProgs + total
         numSongs = numSongs + 1
     else:
-<<<<<<< HEAD
-        print("No songs in directory!")
-=======
         print("Total: " + str(sum(answers.values())) + " Does not match: " + str(total) + "\n")
 
 stackedArray = np.empty(3) #new blank array for combining results
@@ -83,19 +80,11 @@ stackedArray = np.delete(stackedArray, 0, 0)
 
 dict2 = {} #new blank dictionary
 
-for elem in stackedArray: #remove duplicates, by using dictionary
+for elem in stackedArray:
     key = tuple(elem[:-1])
     value = int(elem[-1])
     dict2[key] = dict2.get(key,0) + value
 
-<<<<<<< HEAD
-finalArray = [[*key, value] for key, value in dict2.items()] #convert to array
-print(finalArray)
-
-with open("export.txt", "w") as txt_file: #export to plain array txt
-    for line in finalArray:
-        txt_file.write(" ".join(map(str,line)) + "\n") # works with any number of elements in a line
-=======
 finalArray = [[*key, value] for key, value in dict2.items()]
 finalArray = np.array(finalArray)
 
@@ -109,5 +98,3 @@ print(dash)
 
 # save finalArray to txt file
 np.savetxt('export.txt', finalArray, fmt='%s')
->>>>>>> leighton
->>>>>>> master
