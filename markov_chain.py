084fd8bcb9a86b095ac7c7de38f1d9d2d9926b11
Tarang74 <31427635+Tarang74@users.noreply.github.com>
2020-02-06 21:47:47 +1000

    Now able to input transition length, which generates chords (as index) in array
    Next step: To use generated chord transition array (which is random) to create music. (maybe mapping 24 chords to certain frequencies)

--- markov_chain.py
+++ markov_chain.py
@@ -1,5 +1,7 @@
 # Created by Leighton Swannell
 # February 3, 2020
+# Modified for all chords by Tarang Janawalkar
+# February 5, 2020
 
 import numpy as np
 import os
@@ -12,25 +14,27 @@ states = ["A", "Am", "A#", "A#m",
         "D", "Dm", "D#", "D#m",
         "E", "Em",
         "F", "Fm", "F#", "F#m",
-        "G", "Gm", "G#", "G#m"] #All possible chords that can be combined
-tName = [] #Transition names (eg. A-Bm)
-tOccr = [] #Occurance matrix
-tProb = [] #Probabilities matrix
+        "G", "Gm", "G#", "G#m"] # All possible chords that can be combined
+tName = [] # Transition names (eg. A-Bm)
+tOccr = [] # Occurance matrix
+tProb = [] # Probabilities matrix
+tProbR = [] # Probabilities matrix (rounded for viewing)
 sumArry = []
 sumTcol = []
 xyMtrx = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
 
-for i in states: #for every chord
-    tI = [] #create temporary arrays
+for i in states: # for every chord
+    tI = [] # create temporary arrays
     tJ = []
     tK = []
-    for j in states: #for every chord
-        tI.append(j+"-"+i) #add combination of j and i to temp array
+    for j in states: # for every chord
+        tI.append(j+"-"+i) # add combination of j and i to temp array
         tJ.append(0)
         tK.append(0.0)
-    tName.append(tI) #add temp arrays to transition arrays
+    tName.append(tI) # add temp arrays to transition arrays
     tOccr.append(tJ)
     tProb.append(tK)
+    tProbR.append(tK)
 
 data = finalArray
 for i in range(len(data)):
@@ -45,16 +49,16 @@ for i in range(len(data)):
 x = 0
 y = 0
 z = 0
-for i in range(len(data)): #for every three value array
+for i in range(len(data)): # for every three value array
     for j in range(len(states)):
-        if data[i][0] == states[j]: #where the first letter is a possible chord j
-            x = j #matrix x coordinate
-        if data[i][1] == states[j]: #where the second letter is a possible chord j
-            y = j #matrix y coordinate
-    z = data[i][2] #matrix value at coordinates
+        if data[i][0] == states[j]: # where the first letter is a possible chord j
+            x = j # matrix x coordinate
+        if data[i][1] == states[j]: # where the second letter is a possible chord j
+            y = j # matrix y coordinate
+    z = data[i][2] # matrix value at coordinates
     tOccr[y].insert(x, z)
 
-#create matrix T with values unrounded
+# create matrix T with values unrounded
 f = 0
 for i in range(len(tOccr)):
     for k in range(len(tOccr)):
@@ -63,10 +67,11 @@ for i in range(len(tOccr)):
     if f == 0:
         f = 1
     for j in range(len(tOccr)):
-        tProb[j][i] = round(int(tOccr[j][i]) / f, 4)
+        tProb[j][i] = int(tOccr[j][i]) / f
+        tProbR[j][i] = round(int(tOccr[j][i]) / f, 4)
     f = 0
 
-#XY Position Matrix Output
+# XY Position Matrix Output
 print("\n")
 for i in range(len(tOccr)):
     if i == 0:
@@ -76,7 +81,7 @@ for i in range(len(tOccr)):
         print('{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}'.format(states[0],states[1],states[2],states[3],states[4],states[5],states[6],states[7],states[8],states[9],states[10],states[11],states[12],states[13],states[14],states[15],states[16],states[17],states[18],states[19],states[20],states[21],states[22],states[23]))
         print(dash)
     print("{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<2s}{:<4s}{:<2s}".format(tOccr[i][0], tOccr[i][1], tOccr[i][2], tOccr[i][3], tOccr[i][4], tOccr[i][5], tOccr[i][6], tOccr[i][7], tOccr[i][8], tOccr[i][9], tOccr[i][10], tOccr[i][11], tOccr[i][12], tOccr[i][13], tOccr[i][14], tOccr[i][15], tOccr[i][16], tOccr[i][17], tOccr[i][18], tOccr[i][19], tOccr[i][20], tOccr[i][21], tOccr[i][22], tOccr[i][23],  line, states[i], line))
-#Sum Output
+# Sum Output
 print(dash)
 print(dash)
 print("{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<2s}{:<4s}{:<2s}".format(sumArry[0], sumArry[1], sumArry[2], sumArry[3], sumArry[4], sumArry[5], sumArry[6], sumArry[7], sumArry[8], sumArry[9], sumArry[10], sumArry[11], sumArry[12], sumArry[13], sumArry[14], sumArry[15], sumArry[16], sumArry[17], sumArry[18], sumArry[19], sumArry[20], sumArry[21], sumArry[22], sumArry[23], line,"Total", line))
@@ -89,4 +94,44 @@ for i in range(len(tProb)):
         print(dash)
         print('{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}'.format(states[0],states[1],states[2],states[3],states[4],states[5],states[6],states[7],states[8],states[9],states[10],states[11],states[12],states[13],states[14],states[15],states[16],states[17],states[18],states[19],states[20],states[21],states[22],states[23]))
         print(dash)
-    print("{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<2s}{:<4s}{:<2s}".format(tProb[i][0], tProb[i][1], tProb[i][2], tProb[i][3], tProb[i][4], tProb[i][5], tProb[i][6], tProb[i][7], tProb[i][8], tProb[i][9], tProb[i][10], tProb[i][11], tProb[i][12], tProb[i][13], tProb[i][14], tProb[i][15], tProb[i][16], tProb[i][17], tProb[i][18], tProb[i][19], tProb[i][20], tProb[i][21], tProb[i][22], tProb[i][23], line, states[i], line))
+    print("{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<7}{:<2s}{:<4s}{:<2s}".format(tProbR[i][0], tProbR[i][1], tProbR[i][2], tProbR[i][3], tProbR[i][4], tProbR[i][5], tProbR[i][6], tProbR[i][7], tProbR[i][8], tProbR[i][9], tProbR[i][10], tProbR[i][11], tProbR[i][12], tProbR[i][13], tProbR[i][14], tProbR[i][15], tProbR[i][16], tProbR[i][17], tProbR[i][18], tProbR[i][19], tProbR[i][20], tProbR[i][21], tProbR[i][22], tProbR[i][23], line, states[i], line))
+
+markovMatrix = np.asarray([tProb])
+print(markovMatrix)
+print(dash)
+sum = np.sum(a=markovMatrix, axis=1, dtype=float) # Sum of columns in Markov Matrix
+print("Sum of matrix columns:")
+print(sum)
+print(dash)
+
+start = input("Enter starting note: ") # Starting note
+startMatrix = np.zeros((24,1)) # All other notes have 0 probability
+
+currentProbability = int
+for i in range(len(states)): # Set 1, to row of startMatrix
+    if start == states[i]:
+        startMatrix[i][0] = 1
+        currentProbability = i
+
+print("Starting Transition Probability Matrix:")
+print(markovMatrix[:,currentProbability])
+print(dash)
+multPower = int(input("Number of Transition events: "))
+print(dash)
+print("End Probability Matrix:")
+endMatrix = []
+endMatrix = np.dot(np.linalg.matrix_power(markovMatrix, multPower),startMatrix)
+print(endMatrix)
+print(dash)
+print("Chord Progression based on random selection, using chord indexes:")
+
+statesIndex = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+chordGeneration = np.zeros(multPower, dtype=int)
+for i in range(1, multPower):
+    prob = np.dot(np.linalg.matrix_power(markovMatrix, i), startMatrix)
+    prob = prob.flatten()
+    prob = prob.tolist()
+    chordGeneration[i] = np.random.choice(statesIndex, p=prob)
+    # print(prob) #Each column matrix probability
+
+print(chordGeneration)
